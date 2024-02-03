d2878cd3ae8bf1f0e2e42a2d64135a1255be2bc1
Lodyga <lodyga@o2.pl>
2024-02-03 23:30:46 +0100

    03.02.2024

--- codewars.py
+++ codewars.py
@@ -7043,7 +7043,70 @@ def grow(arr):
 
 
 
+# Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+# https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed/train/python
+"""Description:
+Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+Examples
+replace("Hi!") === "H!!"
+replace("!Hi! Hi!") === "!H!! H!!"
+replace("aeiou") === "!!!!!"
+replace("ABCDE") === "!BCD!""""
+
+
+def replace_exclamation(st):
+    return "".join("!" if i.lower() in "aeoiu" else i for i in st)
+(replace_exclamation("Hi!") , "H!!")
+(replace_exclamation("!Hi! Hi!") , "!H!! H!!")
+(replace_exclamation("aeiou") , "!!!!!")
+(replace_exclamation("ABCDE") , "!BCD!")
+
+import re
+def replace_exclamation(st):
+    return re.sub(r"[aeoiu]", "!", st, flags=re.IGNORECASE)
+
+
+
+
+
+# Fix string case
+# https://www.codewars.com/kata/5b180e9fedaa564a7000009a/train/python
+"""In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+make as few changes as possible.
+if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+For example:
+
+solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+solve("coDE") = "code". Upper == lowercase. Change all to lowercase."""
+
+
+def solve(s):
+    upper_count = len(list(filter(str.isupper, s)))
+    lower_count = len(s) - upper_count
+    return s.upper() if upper_count > lower_count else s.lower()
+(solve("code"), "code")
+(solve("CODe"), "CODE")
+(solve("COde"), "code")
+(solve("Code"), "code")
+
+
+
+
+
 # 
+
+
+
+
+
+
+
+
+
+
 
 
 
